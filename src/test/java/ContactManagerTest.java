79a9146fc1b2025c2c899d68eb8578703b626e49
caglarfindikli <cfindikli@outlook.com>
2021-06-17 16:20:24 +0300

    pom xml changes.

--- src/test/java/ContactManagerTest.java
+++ src/test/java/ContactManagerTest.java
@@ -70,6 +70,15 @@ public class ContactManagerTest {
     }
 
     @Test
+    @DisplayName("Should Create Contact")
+    @EnabledOnOs(value = OS.LINUX, disabledReason = "Should Run only on LINUX")
+    public void shouldCreateContactOnLinux() {
+        contactManager.addContact("John", "Doe", "0123456789");
+        assertFalse(contactManager.getAllContacts().isEmpty());
+        assertEquals(1, contactManager.getAllContacts().size());
+    }
+
+    @Test
     @DisplayName("Test Contact Creation on Developer Machine")
     public void shouldTestContactCreationOnDEV() {
         Assumptions.assumeTrue("DEV".equals(System.getProperty("ENV")));
@@ -111,7 +120,6 @@ public class ContactManagerTest {
         }
 
         @DisplayName("CSV Source Case - Phone Number should match the required Format")
-        @Disabled
         @ParameterizedTest
         @CsvSource({"0123456789", "0123456798", "0123456897"})
         public void shouldTestPhoneNumberFormatUsingCSVSource(String phoneNumber) {
@@ -120,36 +128,16 @@ public class ContactManagerTest {
             assertEquals(1, contactManager.getAllContacts().size());
         }
 
-        @DisplayName("CSV File Source Case - Phone Number should match the required Format")
-        @Disabled
-        @ParameterizedTest
-        @CsvFileSource(resources = "contact-manager/src/test/resources/data.csv")
-        public void shouldTestPhoneNumberFormatUsingCSVFileSource(String phoneNumber) {
-            contactManager.addContact("John", "Doe", phoneNumber);
-            assertFalse(contactManager.getAllContacts().isEmpty());
-            assertEquals(1, contactManager.getAllContacts().size());
-        }
     }
 
-    @DisplayName("Method Source Case - Phone Number should match the required Format")
-    @ParameterizedTest
-    @MethodSource("phoneNumberList")
-    public void shouldTestPhoneNumberFormatUsingMethodSource(String phoneNumber) {
-        contactManager.addContact("John", "Doe", phoneNumber);
-        assertFalse(contactManager.getAllContacts().isEmpty());
-        assertEquals(1, contactManager.getAllContacts().size());
-    }
+
 
     private static List<String> phoneNumberList() {
         return Arrays.asList("0123456789", "0123456798", "0123456897");
     }
 
-    @Test
-    @DisplayName("Test Should Be Disabled")
-    @Disabled
-    public void shouldBeDisabled() {
-        throw new RuntimeException("Test Should Not be executed");
-    }
+
+
 
 
     @AfterEach
